2f027ea6d5e1b9184b92f4459792854d4284fa66
Daniel Gakwaya <wayadn88@gmail.com>
2025-07-21 10:30:33 +0300

    Fix QML module URI and update main.cpp to load from module

--- 4.CustomComponentsAndModules/3-QMLModules/main.cpp
+++ 4.CustomComponentsAndModules/3-QMLModules/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/3-QMLModules/Main.qml"_qs);
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
@@ -14,7 +13,8 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.addImportPath(":/");
-    engine.load(url);
+    engine.loadFromModule("QMLModules", "Main");
 
     return app.exec();
+
 }
